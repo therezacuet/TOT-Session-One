76b55e0c17da7d8dfd7b7bab6ed381870bb7cbf7
Rezaul Islam <rezaulislam.mcc@gmail.com>
2018-04-21 21:37:01 +0600

    Update

--- app/src/main/java/sessionone/tot/com/session_one/activities/Home.java
+++ app/src/main/java/sessionone/tot/com/session_one/activities/Home.java
@@ -1,20 +1,37 @@
 package sessionone.tot.com.session_one.activities;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.widget.ImageView;
 
+import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import sessionone.tot.com.session_one.R;
 
 public class Home extends AppCompatActivity {
 
+    AnimationDrawable animationControler;
+    @BindView(R.id.imageView)
+    ImageView ivAnim;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         ButterKnife.bind(this);
+        customAnimation();
+    }
+
+    private void customAnimation() {
+
+        ivAnim.setImageResource(R.drawable.frameanimation);
+        animationControler=(AnimationDrawable)ivAnim.getDrawable();
+        animationControler.start();
+
     }
 
     @OnClick(R.id.btn_web_view)
